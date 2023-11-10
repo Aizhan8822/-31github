cd8ea5ca237be9d0c6362785e5c17c88747b7116
Aizhan8822 <116110245+Aizhan8822@users.noreply.github.com>
2023-11-10 20:12:58 +0600

    Add files via upload

---
+++ 27.js
@@ -0,0 +1,30 @@
+var i = 0; 
+while (i < 10){ 
+  document.write("hello "); 
+  i = i+1; 
+} 
+for (var j = 0; j < 10; j = j+1){ 
+  document.write("Привет") 
+}
+
+for(var i=0;i<=100;i=i+1){
+    if(i%2==1){document.write(i+<br>)}
+sum=sum+i;}
+}
+}
+document.write(sum);
+
+
+for(var i=0;i<5;i++){for(var j=0;j<5;j++){}}
+
+
+for(var i=0;i<100;i++){if(i%7==0)}
+
+
+for(var i=0;i<7;i++){
+
+}
+
+for(var i=0;i<1000){if(i/7==0</br>==1)}
+
+for
